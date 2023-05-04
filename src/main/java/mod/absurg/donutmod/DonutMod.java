b86131dbc12393dc96109e72dfd6105f0470c69c
Absurg <129305664+Absurg@users.noreply.github.com>
2023-05-05 00:25:09 +0600

    changed changelog.txt deleted comments from DonutMod.java changed dynamic_lights.properties added en_us, ru_ru localization changed recipe for glowing donut (i forgot that glow berries dont exist in 1.16.5 lol) changes in the GlowingDonut.java class new textures .toml file version update (0.1.2)

--- src/main/java/mod/absurg/donutmod/DonutMod.java
+++ src/main/java/mod/absurg/donutmod/DonutMod.java
@@ -20,12 +20,10 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.stream.Collectors;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(DonutMod.MOD_ID)
 public class DonutMod
 {
     public static final String MOD_ID = "donutmod";
-    // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
     public DonutMod() {
@@ -65,20 +63,15 @@ public class DonutMod
 
     private void processIMC(final InterModProcessEvent event)
     {
-        // some example code to receive and process InterModComms from other mods
         LOGGER.info("Got IMC {}", event.getIMCStream().
                 map(m->m.getMessageSupplier().get()).
                 collect(Collectors.toList()));
     }
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
+
     @SubscribeEvent
     public void onServerStarting(FMLServerStartingEvent event) {
-        // do something when the server starts
         LOGGER.info("HELLO from server starting");
     }
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
     @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
     public static class RegistryEvents {
         @SubscribeEvent
--- src/main/java/mod/absurg/donutmod/item/GlowingDonut.java
+++ src/main/java/mod/absurg/donutmod/item/GlowingDonut.java
@@ -1,7 +1,9 @@
 package mod.absurg.donutmod.item;
 
-import net.minecraft.item.*;
-import net.minecraft.potion.EffectInstance; 
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 public class GlowingDonut extends Item {
     public GlowingDonut() {
@@ -9,9 +11,10 @@ public class GlowingDonut extends Item {
                 .tab(ItemGroup.TAB_FOOD)
                 .food(new Food.Builder()
                         .saturationMod(5.3f)
-                        .nutrition(7)
+                        .nutrition(6)
+                        .alwaysEat()
                         .effect(() -> new EffectInstance
-                                (Effects.NIGHT_VISION, 6000, 0), 1).build())
+                                (Effects.NIGHT_VISION, 3600, 0), 1).build())
         );
     }
 }
